d31ccf38830391b9c6d551f5bea14165affb4b35
sebamiro <seba.mi.21@gmail.com>
2024-03-10 14:10:19 +0100

    V1.0 iferr-nvim

--- lua/iferr.lua
+++ lua/iferr.lua
@@ -1,30 +1,57 @@
 local api = vim.api
 
--- TSNode:type() string
--- TSNode:iter_children()
--- TSNode:field()
-
-local function get_function_parent(c_node)
-	if c_node:type() == "func_literal" or c_node:type() == "function_declaration" then
-		return c_node
+local function get_function_parent(node)
+	if node:type() == "func_literal" or node:type() == "function_declaration" then
+		return node
 	end
-	return get_function_parent(c_node:parent())
+	return get_function_parent(node:parent())
+end
+
+local function get_zero_value(node)
+	if node:type() == "pointer_type" then
+		return "nil"
+	end
+	local row, scol, _ = node:start()
+	local _, ecol, _ = node:end_()
+	local type = api.nvim_buf_get_text(0, row, scol, row, ecol, {})[1]
+	if type == "error" then
+		return "err"
+	elseif type == "string" then
+		return '""'
+	elseif type:sub(0, 3):match("int") or type:sub(0, 5):match("float") or type:sub(0, 4):match("uint") then
+		return "0"
+	elseif type:sub(0, 1):match("%u") then
+		return type .. "{}"
+	end
+	return type
+end
+
+local function get_parameter_list_value(node)
+	local params = {}
+	for c in node:iter_children() do
+		if c:type():match("parameter_declaration") then
+			table.insert(params, get_zero_value(c:child(0)))
+		end
+	end
+	return table.concat(params, ", ")
 end
 
 local function get_result_type(func_node)
-	local count = func_node:child_count()
-	local type = func_node:type() == "func_literal"
-	if (type and count <= 3) or (not type and count <= 4) then
-		return nil
+	local result = func_node:field("result")[1]
+	if result:type():match("parameter_list") then
+		return get_parameter_list_value(result)
 	end
-	return "Hola"
+	return get_zero_value(result)
 end
 
 local function get_return_value()
-	local ts_utils = require("nvim-treesitter.ts_utils")
-	local func_node = get_function_parent(ts_utils.get_node_at_cursor(0))
-	print(get_result_type(func_node))
-	return "err"
+	local func_node = get_function_parent(require("nvim-treesitter.ts_utils").get_node_at_cursor(0))
+	local count = func_node:child_count()
+	local type = func_node:type():match("func_literal")
+	if (type and count <= 3) or (not type and count <= 4) then
+		return ""
+	end
+	return " " .. get_result_type(func_node)
 end
 
 local function iferr()
@@ -33,10 +60,10 @@ local function iferr()
 	local ident = line:sub(0, line:find("%S", 0, false) - 1)
 	api.nvim_buf_set_lines(0, row, row, false, {
 		ident .. "if err != nil {",
-		ident .. "\treturn " .. get_return_value(),
+		ident .. "\treturn" .. get_return_value(),
 		ident .. "}"
 	})
-	api.nvim_win_set_cursor(0, {row + 3, ident:len()})
+	api.nvim_win_set_cursor(0, { row + 3, ident:len() })
 end
 
 return {
