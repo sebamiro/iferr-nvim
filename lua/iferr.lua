77f312e4a03131a09c37a5f67496beb4f4902a36
sebamiro <seba.mi.21@gmail.com>
2024-03-19 21:26:33 +0100

    Now using LuaSnips

--- lua/iferr.lua
+++ lua/iferr.lua
@@ -1,73 +1,162 @@
 local api = vim.api
 
+require("luasnip.session.snippet_collection").clear_snippets("go")
+
+local ls = require("luasnip")
+
+local fmta = require("luasnip.extras.fmt").fmta
+local rep = require("luasnip.extras").rep
+
+local s = ls.snippet
+local c = ls.choice_node
+local d = ls.dynamic_node
+local i = ls.insert_node
+local t = ls.text_node
+local sn = ls.snippet_node
+
 local function get_function_parent(node)
-	if node:type():match("func_literal")
-		or node:type():match("function_declaration")
-		or node:type():match("method_declaration") then
+	local func_types = {
+		func_literal = true,
+		function_declaration = true,
+		method_declaration = true
+	}
+
+	if not node or func_types[node:type()] then
 		return node
 	end
 	return get_function_parent(node:parent())
 end
 
-local function get_zero_value(node)
-	if node:type() == "pointer_type" then
-		return "nil"
-	end
-	local row, scol, _ = node:start()
-	local _, ecol, _ = node:end_()
-	local type = api.nvim_buf_get_text(0, row, scol, row, ecol, {})[1]
-	if type == "error" then
-		return "err"
-	elseif type == "string" then
-		return '""'
-	elseif type:sub(0, 3):match("int") or type:sub(0, 5):match("float") or type:sub(0, 4):match("uint") then
-		return "0"
-	elseif type:sub(0, 1):match("%u") then
-		return type .. "{}"
-	end
-	return type
-end
+local zero_values = {
+	int = "0",
+	bool = "false",
+	string = '""',
 
-local function get_parameter_list_value(node)
-	local params = {}
-	for c in node:iter_children() do
-		if c:type():match("parameter_declaration") then
-			table.insert(params, get_zero_value(c:child(0)))
+	error = function(_, info)
+		if info then
+			info.index = info.index + 1
+
+			return c(info.index, {
+				t(info.err_name),
+				t(string.format('errors.Wrap(%s, "%s")', info.err_name, info.func_name)),
+			})
+		else
+			return t("err")
+		end
+	end,
+
+	[function(text)
+		return string.find(text, "*", 1, true) ~= nil
+	end] = function(_, _)
+		return t("nil")
+	end,
+
+	[function(text)
+		return text:match("%u")
+	end] = function(text, info)
+		info.index = info.index + 1
+
+		return c(info.index, {
+			t(text .. "{}"),
+			t(text)
+		})
+	end,
+}
+
+local transform = function(text, info)
+	local condition_matches = function(condition, ...)
+		if type(condition) == "string" then
+			return condition == text
+		else
+			return condition(...)
 		end
 	end
-	return table.concat(params, ", ")
+
+	for condition, result in pairs(zero_values) do
+		if condition_matches(condition, text, info) then
+			if type(result) == "string" then
+				return t(result)
+			else
+				return result(text, info)
+			end
+		end
+	end
+
+	return t(text)
 end
 
-local function get_result_type(func_node)
+local handlers = {
+	parameter_list = function(node, info)
+		local params = {}
+		local count = node:named_child_count()
+
+		for idx = 0, count - 1 do
+			local children = node:named_child(idx)
+			local type_node = children:field("type")[1]
+			table.insert(params, transform(vim.treesitter.get_node_text(type_node, 0), info))
+			if idx ~= count - 1 then
+				table.insert(params, t { ", " } )
+			end
+		end
+		return params
+	end,
+
+	type_identifier = function(node, info)
+		local text = vim.treesitter.get_node_text(node, 0)
+		return { transform(text, info) }
+	end
+}
+
+local function get_result_value(info)
+	local func_node = get_function_parent(vim.treesitter.get_node())
+	if not func_node then
+		vim.notify("No in a function")
+		return t("Error")
+	end
 	local result = func_node:field("result")[1]
-	if result:type():match("parameter_list") then
-		return get_parameter_list_value(result)
+	if result then
+		if handlers[result:type()] then
+			return handlers[result:type()](result, info)
+		end
 	end
-	return get_zero_value(result)
 end
 
-local function get_return_value()
-	local func_node = get_function_parent(require("nvim-treesitter.ts_utils").get_node_at_cursor(0))
-	local count = func_node:child_count()
-	local type = func_node:type():match("func_literal")
-	if (type and count <= 3) or (not type and count <= 4) then
-		return ""
-	end
-	return " " .. get_result_type(func_node)
+local function iferr(args)
+	return sn(nil,
+		get_result_value({
+			index = 0,
+			err_name = args[1][1],
+			func_name = args[2][1],
+		})
+	)
 end
 
-local function iferr()
-	local row = api.nvim_win_get_cursor(0)[1]
-	local line = api.nvim_get_current_line()
-	local ident = line:sub(0, line:find("%S", 0, false) - 1)
-	api.nvim_buf_set_lines(0, row, row, false, {
-		ident .. "if err != nil {",
-		ident .. "\treturn" .. get_return_value(),
-		ident .. "}"
+local function setup()
+	ls.add_snippets("go", {
+		s(
+			"efi",
+			fmta(
+				[[
+		<val>, <err> := <f>(<args>)
+		if <err_same> != nil {
+			return <result>
+		}
+		<finish>
+		]],
+				{
+					val = i(1),
+					err = i(2, "err"),
+					f = i(3),
+					args = i(4),
+					err_same = rep(2),
+					result = d(5, iferr, { 2, 3 }),
+					finish = i(0),
+				}
+			)
+		),
 	})
-	api.nvim_win_set_cursor(0, { row + 3, ident:len() })
 end
 
 return {
-	iferr = iferr
+	setup = setup,
 }
